5dad8f757b1a6f7979c8a7b0a4d14bb312735d26
E.T.Cook <e@etc.io>
2013-07-04 21:01:13 -0500

    Fixes for less/css
    Signed-off-by: E.T.Cook <e@etc.io>

--- sections/DepthCharge/section.php
+++ sections/DepthCharge/section.php
@@ -15,7 +15,6 @@ V3: true
 class etcDepthCharge extends PageLinesSection {
 
 	function section_styles(){
-		wp_enqueue_script('etcDepthCharge', $this->base_url.'/js/etcDepthCharge.min.js',array('jquery'),'1.0b',true);
 		wp_enqueue_script('skrollr', $this->base_url.'/js/skrollr.js',array(),'0.6.8',true);
 
 		$sprites = ($this->opt('sprite_count')) ? $this->opt('sprite_count') : $this->default_sprites;
@@ -25,6 +24,7 @@ class etcDepthCharge extends PageLinesSection {
 				break;
 			endif;
 		}
+		wp_enqueue_script('etcDepthCharge', $this->base_url.'/js/etcDepthCharge.js',array('jquery'),'1.0b',true);
 	}
 
 	function section_head( $clone_id ){
