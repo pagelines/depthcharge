efbbdea14cb24ec169df4e7b61bd100d7e5b0a36
E.T.Cook <e@etc.io>
2013-07-05 09:47:58 -0500

    Fixed reference to the min js file
    Signed-off-by: E.T.Cook <e@etc.io>

--- sections/DepthCharge/section.php
+++ sections/DepthCharge/section.php
@@ -16,7 +16,7 @@ class etcDepthCharge extends PageLinesSection {
 
 	function section_styles(){
 		wp_enqueue_script('skrollr', $this->base_url.'/js/skrollr.js',array(),'0.6.8',true);
-		wp_enqueue_script('etcDepthCharge', $this->base_url.'/js/etcDepthCharge.js',array('jquery'),'1.0b',true);
+		wp_enqueue_script('etcDepthCharge', $this->base_url.'/js/etcDepthCharge.min.js',array('jquery'),'1.0b',true);
 		$sprites = ($this->opt('sprite_count')) ? $this->opt('sprite_count') : $this->default_sprites;
 		for($i = 1; $i <= $sprites; $i++){
 			if( $this->opt('sprite'.$i.'_type') == 'text' ):
